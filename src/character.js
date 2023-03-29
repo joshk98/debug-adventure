b54d38cf1d50e849aa03429636bba9ef06ba26ca
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-03-29 22:18:26 +0100

    Project complete

--- src/character.js
+++ src/character.js
@@ -1,5 +1,5 @@
 /* eslint-disable func-names */
-function Character() {
+function Character(config) {
   this.name = config.name;
   this.health = config.health;
   this.maxHealth = config.maxHealth;
@@ -12,22 +12,22 @@ Character.prototype = {
   },
 };
 
-Character.prototype = {
-  _takeDamage: function (damage) {
-    this.health = damage;
-    if (this.health < 0) {
-      this.health = 0;
-    }
+Character.prototype._takeDamage = function (damage) {
+  this.health -= damage;
+  if (this.health < 0) {
+    this.health = 0;
   }
-  _heal: function (health) {
-    this.health += health;
-    if (this.health < this.maxHealth) {
-      this.health = this.maxHealth;
-    }
-  },
-  speak function () {
-    return this.dialog;
-  },
+};
+
+Character.prototype._heal = function (potion) {
+  this.health += potion;
+  if (this.health > this.maxHealth) {
+    this.health = this.maxHealth;
+  }
+};
+
+Character.prototype.speak = function () {
+  return this.dialogue;
 };
 
 module.exports = Character;
